5e141c0fb80a60d322c35db43d052e6205dd51e0
Hajid Alkindi <105734161+MaskedAlkindi@users.noreply.github.com>
2024-06-23 01:47:06 +0400

    Fixed ios directory

--- UWBSampleApp/ios/UWBSampleApp/UWBSampleApp-Bridging-Header.h
+++ UWBSampleApp/ios/UWBSampleApp/UWBSampleApp-Bridging-Header.h
@@ -1,13 +1,3 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
-
-
-// Import the React Native code we want to use.
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-// For Expo Core unimodules.
-#import <EXCore/EXExportedModule.h>
-#import <EXCore/EXModuleRegistryConsumer.h>
